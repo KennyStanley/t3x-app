62a8f562b539db370a26d0fa677c1dfb9b217e86
Kenny Stanley <kpstanleyjr@gmail.com>
2022-08-14 00:19:40 -0500

    feat: added machine router to app routers

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -4,11 +4,13 @@ import superjson from 'superjson'
 
 import { exampleRouter } from './example'
 import { protectedExampleRouter } from './protected-example-router'
+import { machineRouter } from './machine-router'
 
 export const appRouter = createRouter()
   .transformer(superjson)
   .merge('example.', exampleRouter)
   .merge('question.', protectedExampleRouter)
+  .merge('machine.', machineRouter)
 
 // export type definition of API
 export type AppRouter = typeof appRouter
